6f1c1ac29f70452836ccd2ae5356f79f4fedfca9
Adron Hall <adronhall@gmail.com>
2014-11-17 18:04:33 -0800

    Added receipt and clean up code for queue.

--- server.js
+++ server.js
@@ -8,32 +8,50 @@ var
 
 var server = new Hapi.Server(process.env.PORT || 3000);
 
-function receiveSqsMessage() {
-  "use strict";
-
-  AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: awsRegion
-  });
-  sqs = new AWS.SQS();
-
-}
-
 
 server.route({
   method: 'POST',
   path: '/hi',
   handler: function (request, reply) {
 
-    server.log('response: ', request.payload);
-    server.log('testing1');
 
-    console.log('response: ', request.payload);
-    console.log('testing2');
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: awsRegion
+    });
+    sqs = new AWS.SQS();
 
-    reply('Hello response' + request.payload);
+    server.log('response: ', request.payload.name);
+    server.log('Starting receive message.');
 
+    sqs.receiveMessage({
+      QueueUrl: queueUri,
+      MaxNumberOfMessages: 1, // how many messages to retrieve?
+      VisibilityTimeout: 15, // how long to lock the job.
+      WaitTimeSeconds: 3 // how long to wait for message.
+    }, function(err, data) {
+      // If there are any messages to get
+      if (data.Messages) {
+        // Getting first message, which there would only be one at this point.
+        var message = data.Messages[0],
+          body = JSON.parse(message.Body);
+
+        server.log('body: ' + body);
+
+        var removeFromQueue = function(message) {
+          sqs.deleteMessage({
+            QueueUrl: queueUri,
+            ReceiptHandle: message.ReceiptHandle
+          }, function(err, data) {
+            err && console.log(err);
+          });
+        };
+
+      }
+    });
+
+    reply('Hello response' + request.payload.name);
   }
 });
 
